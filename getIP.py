cbf5c8bb21ea1383f4c6fa3d67768882abb5082e
hamson <hamish.songsmith@icloud.com>
2020-04-12 10:17:21 +1000

    SIngles out IP address

--- getIP.py
+++ getIP.py
@@ -5,6 +5,6 @@ t = time.localtime()
 start_time = time.strftime("%H:%M:%S", t)
 print(start_time)
 
-interface = netifaces.ifaddresses('wlan0')
+interface = netifaces.ifaddresses('wlan0') # Needs to be wlan0 for rpi // en0 for MBP
 
-print(interface[netifaces.AF_INET])
+print(interface[netifaces.AF_INET][0]['addr'])
